214f1acd17460b2e7e48b877779a056f94542317
uruta <waltergagate443@gmail.com>
2024-08-04 07:47:46 +0800

    feat(cookie): Added more config settings for cookie

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.post("/", (req, res) => {
 
     if(uname){
         const token = jwt.sign({user: process.env.USER}, process.env.SECRET); 
-        res.cookie('token', token, {httpOnly: true, sameSite: 'Lax'}); 
+        res.cookie('token', token, {httpOnly: false, secure: true, sameSite: 'none', maxAge: '60 * 60', path: '/'}); 
         res.send(token); 
     }
     else{
